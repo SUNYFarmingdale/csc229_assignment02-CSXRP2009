1794f08d9e038810e94bb146a3a0516bfc566da7
CSXRP <CSXRP@LAPTOP-ITVH3M10>
2023-02-09 14:21:08 -0500

    Changes made and comments explaining the code

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -4,6 +4,7 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
+import java.util.Scanner;
 public class DriverClass {
 	
 	public static void main(String[] args) {
@@ -13,12 +14,36 @@ public class DriverClass {
 		
 		// ToDo 7: Add a toString method for Student class
 		
-		Student std1= new Student("James", 20);
+		// Takes input from the user by using the scanner class and stores them in variables	 
+		Scanner sc = new Scanner(System.in);
+                // Asks the user user to his name
+                System.out.print("Enter the student's name: ");
+                // Goes to the next line
+                String name = sc.nextLine();
+                // asks for age of student
+                System.out.print("Enter the student's age: ");
+                // goes to the next short
+                short age = sc.nextShort();
+                // goes to the next line
+                sc.nextLine();
+                // asks for more user input
+                System.out.print("Enter the student's address: ");
+                //goes to the next line
+                String address = sc.nextLine();
+                // asks for more user input
+                System.out.print("Enter the student's GPA: ");
+                // goes to the next float
+                float gpa = sc.nextFloat();
+               // creates student object and takes in 
+               Student std1 = new Student(name, age);
+               // sets the GPA for student
+               std1.setGpa(gpa);
+               
+               System.out.println(std1);
+               // closes opened scanner
+               sc.close();
 		
-		// ToDo 8: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
 		
-		System.out.println(std1);
 		
 		// ToDo 9: add comments and explain your code
 		
